5a7a3d4e2991547794efe49287d96a1498e7fa26
David Gongola <davidgongola5@gmail.com>
2023-10-30 18:58:51 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import navbar from './navbar';
 
 
 function App() {
   return (
     <div className="App">
+        <navbar /> {navbar()}
         <h1>CS 230L</h1>
         <h2>Section - 02</h2>
         <p>WVU ID: 800357886</p>
         <p>Hi I am David Gongola</p>
+
     </div>
   );
 }
